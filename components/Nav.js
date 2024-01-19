9fa68ad6d9b99ee26bec97de39325d00ebd528fb
PyAde <liodeo23@gmail.com>
2024-01-20 00:51:25 +0800

    WB

--- components/Nav.js
+++ components/Nav.js
@@ -1,25 +1,15 @@
-// icons
+import React, { useState } from 'react';
 import { HiHome, HiWindow, HiEnvelope } from 'react-icons/hi2';
-import styled from 'styled-components';
-
-// nav data
-export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'tugas', path: '/tugas', icon: <HiWindow /> },
-  { name: 'report-tugas', path: '/report-tugas', icon: <HiEnvelope /> },
-];
-
-// next link
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-// StyledLink adalah komponen styled-component yang akan mengatur tampilan link
-const StyledLink = styled.a`
-  color: ${(props) => (props.isActive ? 'accentColor' : 'white')};
-  &:hover {
-    color: accentColor;
-  }
-`;
+
+// icons
+export const navData = [
+  { name: 'home', path: '/', icon: <HiHome /> },
+  { name: 'tugas', path: '/tugas', icon: <HiWindow /> },
+  { name: 'report', path: '/report-tugas', icon: <HiEnvelope /> },
+];
 
 const Nav = () => {
   const router = useRouter();
@@ -27,13 +17,25 @@ const Nav = () => {
 
   return (
     <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
-      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-20 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-black backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-20 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'
+      style={{ boxShadow: '2px 4px 4px 8px rgba(0.1, 0, 0, 0.5)',
+      border: '2px solid white' }}>
         {navData.map((link, index) => (
-          <Link className={`${link.path === pathname && 'text-yellow-500'}`} href={link.path} key={index}>
-            {/* Menggunakan StyledLink dan mempassing prop isActive */}
-            <StyledLink isActive={link.path === pathname} className={`relative flex items-center group hover:text-yellow-500`}>
-              <div>{link.icon}</div>
-            </StyledLink>
+          <Link className={`${link.path === pathname && 'text-yellow-700'} relative flex items-center group hover:text-yellow-700
+            transition-all duration-300`} href={link.path} key={index}>
+
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-black relative flex text-white items-center p-[6px] rounded-[3px]'>
+                <div className='text-[12px] leading-none font-semibold capitalize'>{link.name}</div>
+                {/* triangle */}
+                <div className='border-solid border-l-black border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+                </div>
+              </div>
+              {/* icon  */}
+            <div>
+              {link.icon}
+            </div>
           </Link>
         ))}
       </div>
@@ -41,4 +43,7 @@ const Nav = () => {
   );
 };
 
+
+
 export default Nav;
+
--- components/Transition.js
+++ components/Transition.js
@@ -1,3 +1,6 @@
+
+
+
 const Transition = () => {
   return (
     <>
